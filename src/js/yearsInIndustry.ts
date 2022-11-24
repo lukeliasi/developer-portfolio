c5ad03ad070208f4a071d28775c6c02c8b92a98a
Luke Liasi <lukeliasi@gmail.com>
2022-11-24 02:25:55 +0000

    implement better tooltip and about page

--- src/js/yearsInIndustry.ts
+++ src/js/yearsInIndustry.ts
@@ -1,6 +1,4 @@
 import { differenceInYears } from "./differenceInYears";
 import globalData from "../data/global.json";
 
-export function yearsInIndustry() {
-  return differenceInYears(new Date(globalData.careerStartDate), new Date());
-} 
+export const yearsInIndustry = differenceInYears(new Date(globalData.careerStartDate), new Date());
