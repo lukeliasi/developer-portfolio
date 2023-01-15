fc300a5f82994a1c317d36ed2529b2d145ba7346
Luke Liasi <lukeliasi@gmail.com>
2023-01-15 23:43:21 +0000

    finish portfolio items + automate buzzwords

---
+++ src/js/buzzwordHighlight.js
@@ -0,0 +1,53 @@
+/**
+ * These words are buzzwords that should be highlighted
+ */
+const buzzwords = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "React.js",
+  "PHP",
+  "Next.js",
+  "Node.js",
+  "Express.js",
+  "Express",
+  "Node",
+  "HTML",
+  "CSS",
+  "Sass",
+  "Tailwind CSS",
+  "TailwindCSS",
+  "Python",
+  "MongoDB",
+  "PostgreSQL",
+  "MySQL",
+  "SQL",
+  "NoSQL",
+  "Selenium",
+  "Mocha",
+  "Linux",
+  "Docker",
+  "AWS",
+  "Git",
+  "Adobe XD",
+  "Adobe Photoshop",
+  "Adobe Illustrator",
+  "Figma",
+  "WordPress",
+  "Directus",
+  "Directus CMS",
+  "SanityCMS",
+  "SQL/NoSQL",
+  "REST/GraphQL",
+  "Node/Express.js",
+  "EJS",
+];
+
+const paragraphs = document.querySelectorAll("p");
+
+for (const word of buzzwords) {
+  const regex = new RegExp(`(\\s|^)(${word})([.,/()"']?)(\\s|$)`, 'g');
+  for (const element of paragraphs) {
+    element.innerHTML = element.innerHTML.replace(regex, (match, p1, p2, p3, p4) => `${p1}<span class="buzzword">${p2}</span>${p3}${p4}`);
+  }
+}
