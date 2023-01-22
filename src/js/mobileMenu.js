55e9a5386146388dccf2838a8a4f684fa9be037a
Luke Liasi <lukeliasi@gmail.com>
2023-01-22 13:25:07 +0000

    Make home page fixed height + fixes on mobile menu so you can't scroll it when it's open

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -6,10 +6,14 @@ const menuCloseIcon = document.querySelector(".menu-close-icon");
 if (toggleButton) {
   toggleButton.addEventListener("click", function() {
     if (menu.classList.contains("menu-open")) {
+      // Close menu
+      document.body.classList.remove("menu-open");
       menu.classList.remove("menu-open");
       menuBurgerIcon.style.display = "block";
       menuCloseIcon.style.display = "none";
     } else {
+      // Open menu
+      document.body.classList.add("menu-open");
       menu.classList.add("menu-open");
       menuCloseIcon.style.display = "block";
       menuBurgerIcon.style.display = "none";
