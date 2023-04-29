d9e4dc71533b8b7dbf7b76dc5a0e7efc51493e14
Luke Liasi <lukeliasi@gmail.com>
2023-04-29 02:20:36 +0100

    Add more blog content and layout improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './remark-reading-time.mjs';
+
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: "https://lukeliasi.com",
-  integrations: [mdx()]
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime]
+  },
 });
