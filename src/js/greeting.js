7fc17af7f1926134a442c84df8a95dbd4e548ed5
Luke Liasi <lukeliasi@gmail.com>
2023-08-21 23:16:22 +0100

    clarify greeting

--- src/js/greeting.js
+++ src/js/greeting.js
@@ -5,21 +5,21 @@ const today = new Date();
 const currentHour = today.getHours();
 
 export function greeting() {
-  // 4am - 12pm
+  // 4am - 11:59am
   if (currentHour >= 4 && currentHour < 12) {
     return "Good morning!";
   }
 
-  // 12pm - 6pm
+  // 12pm - 5:59pm
   if (currentHour >= 12 && currentHour < 18) {
     return "Good afternoon!";
   }
 
-  // 6pm - 11pm
+  // 6pm - 11:59pm
   if (currentHour >= 18 && currentHour <= 23) {
     return "Good evening!";
   }
 
-  // Between 12am and 4am when it doesn't feel right to say "Good morning"!
+  // Between 12am and 3:59am when it doesn't feel right to say "Good morning"!
   return "Hey there!";
 }
